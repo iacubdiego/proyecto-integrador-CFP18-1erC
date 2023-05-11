6d75f502a510dc70fdaddce4aa7c96ab6b24c12a
Diego Iacub <105656485+sombrerodiego@users.noreply.github.com>
2023-05-11 11:20:38 -0300

    CSS y Estructura HTML

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const path = require('path')
 const express = require('express')
-
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'views/index.html'))
 })
 
-app.listen(3000, () => 
-    console.log("Servidor Corriendo")
-);
+app.listen(PORT, () => {
+  console.log('http://localhost:'+PORT);
+});
