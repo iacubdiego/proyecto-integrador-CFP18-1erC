94bfb055b9447c48d48f90980995d312cdff13e9
Lotito30 <dominguezje21@gmail.com>
2023-05-31 10:50:39 -0300

    creacion de controllers, routes y actualizar app.js con metodos

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const path = require('path')
 const express = require('express')
+
 const app = express()
+
 const PORT = process.env.PORT || 3000
+
+const mainRoutes = require('./routes/mainRoutes')
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views/index.html'))
-})
+app.use('/',mainRoutes)
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'views/index.html'))
+// })
 
 app.listen(PORT, () => {
   console.log('http://localhost:'+PORT);
---
+++ routes/mainRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const mainRoutes = express.Router()
+
+const mainController = require('../controllers/mainController')
+
+mainRoutes.get('/',mainController.index)
+
+module.exports = mainRoutes
