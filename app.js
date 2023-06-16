682f8c030acbecfc7df58aa007c8661a51be52aa
lotito30 <dominguezje21@gmail.com>
2023-06-16 12:19:31 -0300

    clase 16_06 creando CRUD

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
 const PORT = process.env.PORT || 3006
-const mainRoutes = require('./routes/mainRoutes')
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set('views', __dirname +'/views');
 app.set('view engine', 'ejs');
 
+const mainRoutes = require('./routes/mainRoutes')
 app.use('/',mainRoutes)
+
+const productsRoutes = require('./routes/productsRoutes')
+app.use('/products',productsRoutes)
 
 // configuarcion de public static
 app.use(express.static('public'));
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,30 +1,11 @@
-let productList = [
-  {
-      id: 1,
-      nombre : "Auto Blanco",
-      descripcion : "Mejor motor del mercado",
-      precio : 20000,
-      enVenta : true
-  },
-  {
-      id: 2,
-      nombre : "Auto Blanco",
-      descripcion : "El mas Buscado",
-      precio : 20000,
-      enVenta : true
-  },
-  {
-      id: 3,
-      nombre : "Auto Blanco",
-      descripcion : "5 ptas, baul ancho",
-      precio : 20000,
-      enVenta : true
-  }
-  ]
+const path = require('path')
+const fs = require('fs')
+const productListPath = path.resolve(__dirname,"../database/products.json");
+const productList = JSON.parse(fs.readFileSync(productListPath,'utf-8'));
 
 const mainController = {
     index:(req, res) => {
-        res.render("index", { productList: productList })
+        res.render("index", { products : productList })
       }
 }
 
