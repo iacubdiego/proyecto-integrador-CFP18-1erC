82f6ce737b4c04c2d39631435617db3f7ad7a814
arielxct <campostula@gmail.com>
2023-06-18 16:33:57 -0300

    Agregue imagenes

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -10,51 +10,60 @@ const productsController = {
         res.render("index", { products : productList })
     },
     detalleById : (req,res)=>{
-        let id = req.params.id
-        res.send("Get product by ID" + id)
+        let id = req.params.id;
+        res.send("Get product by ID" + id);
     },
+    // crea un producto y lo guarda
     createProduct : (req , res) => {
-        res.render("products/createProducts")
+        res.render("products/createProducts");
     },
     storeProduct : (req , res) => {
-        let product = req.body
-        productList.push(product)
-        fs.writeFileSync(productListPath,JSON.stringify(productList,null,2))
-        res.redirect("/products")
+        let product = req.body;
+        productList.push(product);
+        fs.writeFileSync(productListPath,JSON.stringify(productList,null,2));
+        res.redirect("/products");
     },
+    //------------------------
+    // edita un producto y hace un update
     editProduct : (req , res) => {
-        let id = req.params.id
-        let product = productList.find(product => product.id == id)
-        res.render("products/editProduct", {product : product})
+        let id = req.params.id;
+        let product = productList.find(product => product.id == id);
+        res.render("products/editProduct", {product : product});
+        
     },
     updateProduct : (req , res) => {
-        let id = req.params.id
-        let newProduct = req.body
-        newProduct.id = id
+        let id = req.params.id;
+        let newProduct = req.body;
+        newProduct.id = id;
         for (let index = 0; index < productList.length; index++) {
-            const elements = productList[index]
+            const elements = productList[index];
             if (elements.id == id){
-                productList[index] = newProduct
+                productList[index] = newProduct;
+               // productList.put(newProduct);
             }
         }
-        fs.readFileSync(productListPath,JSON.stringify(productList,null,2))
-        res.redirect("/products")
+        
+        fs.readFileSync(productListPath,JSON.stringify(productList,null,2));
+        res.redirect("/products");
     },
+    // ------------------------
+    // borra un producto segun el id pasado
     deleteProduct : (req,res) => {
-        let id = req.params.id
+        let id = req.params.id;
 
         console.log('deleteProduct' + id);
 
         for (let index = 0; index < productList.length; index++) {
-            const elements = productList[index]
+            const elements = productList[index];
             if (elements.id == id){
-                productList.splice(index, 1)
+                productList.splice(index, 1);
             }
         }
 
-        fs.readFileSync(productListPath,JSON.stringify(productList,null,2))
-        res.redirect("/products")
+        fs.readFileSync(productListPath,JSON.stringify(productList,null,2));
+        res.redirect("/products");
     }
+    //-----------------------------------
 
 }
 
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -2,14 +2,18 @@ const express = require('express')
 
 const productsRoutes = express.Router()
 
-const productsController = require('../controllers/productsControlers')
-
+const productsController = require('../controllers/productsController')
+// muestra todos los productos
 productsRoutes.get('/', productsController.mostrarProductos)
+// muestra un pruducto segun el id
 productsRoutes.get('/detalle/:id', productsController.detalleById);
+// crea un producto y lo guarda mediante el metodos POST
 productsRoutes.get('/create',productsController.createProduct)
 productsRoutes.post('/',productsController.storeProduct)
+// Edita un producto segun el Id pasado y lo regraba mediante el metodo PUT
 productsRoutes.get('/edit/:id',productsController.editProduct)
 productsRoutes.put('/:id',productsController.updateProduct)
+// borra un producto
 productsRoutes.delete('/:id',productsController.deleteProduct)
 
 module.exports = productsRoutes
